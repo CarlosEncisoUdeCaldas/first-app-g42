4ea55e156f7519d8203e42d27a2ef4fff027eabb
CarlosEncisoUdeCaldas <carlosenciso.tic@ucaldas.edu.co>
2022-10-22 14:40:24 -0500

    ok funcion find desde otro archivo

--- src/data/users.js
+++ src/data/users.js
@@ -1,28 +1,51 @@
 //en javascript podemos hacer exportaciones por defecto y de manera individual
 
 const users = [
-    { 
-        id: 1, 
-        nombre: 'Gato', 
-        apellido: 'Felino'
-    },
-    { 
-        id: 2, 
-        nombre: 'Perro', 
-        apellido: 'Can'
-    },
-    { 
-        id: 3, 
-        nombre: 'Loro', 
-        apellido: 'Ave'
-    }
+  {
+    id: 1,
+    nombre: "Gato",
+    apellido: "Felino",
+  },
+  {
+    id: 2,
+    nombre: "Perro",
+    apellido: "Can",
+  },
+  {
+    id: 3,
+    nombre: "Loro",
+    apellido: "Ave",
+  },
+  {
+    id: 4,
+    nombre: "Tony",
+    apellido: "Stark",
+  },
 ];
 
 export const numeroDelGrupo = 42;
 
 //funcion saludo que voy a exportar
 export function saludo() {
-    return 'Hola grupo 42'
+  return "Hola grupo 42";
 }
+
+//funcion que busca un elemento dentro del arrays users por el id
+// export const getElementById = function (id) {
+//   return users.find(function (item) {
+//     return item.id === id;
+//   });
+// };
+
+//misma funcion tipo flecha de arriba tipo flecha
+export const getElementById = (id) => users.find((item) => item.id === id);
+
+//funcion que busca un elemento del arrays users por el nombre
+//funcion declarada de manera tradicional
+export const getElementByName = function (nombre) {
+  return users.find(function (item) {
+    return item.nombre === nombre;
+  });
+};
 
 export default users;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import users from "./data/users"; //importacion por  defecto
+import users , {getElementById, getElementByName} from "./data/users"; //importacion por  defecto
 console.log(users);
 
 //Funcion find
@@ -15,11 +15,23 @@ console.log(users);
 // const getElementById = (id) => users.find( (item) => item.id === id); //funciones con tipo flecha completas
 
 //funcion tradicional completa
-const getElementById = function (id) {
-  return users.find(function (item) {
-    return item.id === id;
-  });
-};
+// const getElementById = function (id) {
+//   return users.find(function (item) {
+//     return item.id === id;
+//   });
+// };
 
 console.log("El id que corresponde al valor 3 es: ");
-console.log(getElementById(1));
+console.log( getElementById (1) );
+console.log( getElementByName ('Loro') );
+
+const result = getElementById (5)
+// const { nombre, id, apellido } = getElementById (1) //desestructuracion directa del objeto resultante
+// console.log( `El Elemento encontra es ${nombre} ${apellido} con id No ${id}`);  //template string con back-tic
+
+if (result) {
+    const { nombre , apellido } = result
+    console.log( `Hola ${nombre} ${apellido}, bienvenido a la plataforma`);  //template string con back-tic
+} else {
+    console.log('Lo sentimos, no tiene privilegios para esta plataforma');
+}
